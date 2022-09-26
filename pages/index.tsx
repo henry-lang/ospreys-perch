86b0691a2b1229dd663c6d296e3838f4e01c2869
Henry Langmack <Henry.langmack@gmail.com>
2022-09-26 15:31:32 -0400

    some intitial progress

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { NextPage } from 'next'
+import aerial from '../public/pictures/aerial.webp'
+
+const Home: NextPage<{}> = () => {
+
+}
+
+export default Home
